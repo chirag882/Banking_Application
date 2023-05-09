9972967c39ee200759530c4d15412b09cb28cf1b
AadityaNayak <aadityanayak7@gmail.com>
2023-05-10 00:27:05 +0530

    Changed Quick cash labels to buttons

--- src/Quick.java
+++ src/Quick.java
@@ -5,6 +5,43 @@ import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 
 public class Quick {
+
+    public void styleButton(JButton b) {
+        Color bgColor = Color.decode("#59043f");
+        Color hoverColor = Color.decode("#c40a8b");
+        Color pressColor = new Color(153, 153, 153);
+        Font font = new Font("Arial", Font.BOLD, 16);
+
+        b.setForeground(Color.WHITE);
+        b.setBorderPainted(false);
+        b.setContentAreaFilled(false);
+        b.setOpaque(true);
+        b.setBackground(bgColor);
+        b.setForeground(Color.WHITE);
+        b.setFont(font);
+
+        b.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                b.setBackground(hoverColor);
+                b.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            public void mouseExited(MouseEvent e) {
+                b.setBackground(bgColor);
+                b.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+                b.setForeground(Color.WHITE);
+
+            }
+
+            public void mousePressed(MouseEvent e) {
+                b.setBackground(pressColor);
+            }
+
+            public void mouseReleased(MouseEvent e) {
+                b.setBackground(hoverColor);
+            }
+        });
+    }
     public void quickView(int id) throws SQLException {
 
         Operations oper = new Operations();
@@ -18,27 +55,33 @@ public class Quick {
         hlabel.setHorizontalAlignment(JLabel.CENTER);
         hlabel.setFont(new Font("Rockwell", Font.BOLD, 25));
         frame.add(hlabel);
-        JLabel two = new JLabel("< 200");
+        JButton two = new JButton("< 200");
         two.setBounds(30, 250, 200, 30);
         two.setFont(txt);
-        JLabel five = new JLabel("500 >");
+        styleButton(two);
+        JButton five = new JButton("500 >");
         five.setBounds(350, 250, 200, 30);
         five.setHorizontalAlignment(JLabel.RIGHT);
         five.setFont(txt);
-        JLabel ten = new JLabel("< 1000");
+        styleButton(five);
+        JButton ten = new JButton("< 1000");
         ten.setBounds(30, 350, 200, 30);
         ten.setFont(txt);
-        JLabel twenty = new JLabel("2000 >");
+        styleButton(ten);
+        JButton twenty = new JButton("2000 >");
         twenty.setBounds(350, 350, 200, 30);
         twenty.setHorizontalAlignment(JLabel.RIGHT);
         twenty.setFont(txt);
-        JLabel fifty = new JLabel("< 5000");
-        fifty.setBounds(30, 450, 250, 30);
+        styleButton(twenty);
+        JButton fifty = new JButton("< 5000");
+        fifty.setBounds(30, 450, 200, 30);
         fifty.setFont(txt);
-        JLabel hundred = new JLabel("10000 >");
+        styleButton(fifty);
+        JButton hundred = new JButton("10000 >");
         hundred.setBounds(350, 450, 200, 30);
         hundred.setHorizontalAlignment(JLabel.RIGHT);
         hundred.setFont(txt);
+        styleButton(hundred);
         frame.add(two);
         frame.add(five);
         frame.add(ten);
